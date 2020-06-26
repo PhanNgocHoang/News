5bced62d020c79133f1eae2cb484223fa8b461d3
quananh <anhquan31299@gmail.com>
2020-06-26 17:31:35 +0700

    react-node

--- API/app.js
+++ API/app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const logger = require('morgan') 
+const logger = require('morgan')
 
 //connect database
 
 app.use(logger('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+var cors = require('cors')
 
-app.use('/', (req, res)=>{
-    res.send('server')
+app.use('/test', cors(), (req, res) => {
+    res.status(200).json({ server: "server" })
 })
 module.exports = app
